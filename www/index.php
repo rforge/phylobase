b95ac20f2e43170955275e22acd1ed0304d3c327
pdc <pdc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-19 20:15:32 +0000

    Update home page with installation instruction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylobase@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,17 +33,17 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+The phylobase package seeks to provide a set of S4 classes and methods for representing and manipulating phylogenetic trees and data in R.
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> The package can be install, on a good day, using the following commands at the R prompt: </p>
+
+<p>For windows users: install.packages("phylobase",repos="http://r-forge.r-project.org")</p>
+
+<p></p>
+
+<p>For everyone else: install.packages("phylobase", repos="http://r-forge.r-project.org", type = 'source')</p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
