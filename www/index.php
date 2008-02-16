2afaea5da7478c9dbe0bbe0d3d328f486bb59dc8
pdc <pdc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-16 03:34:36 +0000

    Update to the web page and tweaks to ensure it validates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylobase@109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -16,19 +16,17 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
 
-<! --- R-Forge Logo --- >
+<!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
 <a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
-
-<!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
 The phylobase package seeks to provide a set of S4 classes and methods for representing and manipulating phylogenetic trees and data in R.
@@ -38,33 +36,36 @@ The phylobase package seeks to provide a set of S4 classes and methods for repre
 <h2>Package resources</h2>
 <ul>
 <li>To install the current release version (0.3, rev. 104): download one of
-the following and install as appropriate for your platform.
-(<strong>phylobase does not currently build on 64-bit *nix.
-We are working to fix this.  Please contact us if you need
-phylobase for this platform, as it may encourage us to work harder.</strong>)
-(The current version requires ape version 2.1: MacOS users will
+the following and install as appropriate for your platform.</li>
+<li>The current version requires ape version 2.1: MacOS users will
 need to specify <tt>install.packages("ape",type="source")</tt>,
 and have development tools installed,
-to get the right version.)
+to get the right version.)</li>
+</ul>
+
 <ul>
 <li><a href="misc/phylobase_0.3.tar.gz">Source</a> (for Linux, Unix, or MacOS/Windows with development
-tools installed)</a></li>
+tools installed)</li>
 <li><a href="misc/phylobase_0.3.zip">Windows binary</a></li>
-<li><strong>add MacOS binary here</strong>
-</ul></li>
-<li>To install the current nightly snapshot (on a good day):
-<ul>
-<li>Windows or *n*x users: <tt>install.packages("phylobase",repos="http://r-forge.r-project.org")</tt></li>
-<li>MacOS users: <tt>install.packages("phylobase", repos="http://r-forge.r-project.org", type = 'source')</tt> (because R-forge does not build MacOS binaries: 
-this will also work for Linux/Unix, where <tt>type='source'</tt> is the
-default)</li>
+<li><a href="misc/phylobase_0.3_R_i386-apple-darwin8.10.1.tar.gz">MacOS <strong>Intel</strong> binary</a> If you require a PowerPC binary please ask on the developer list.</li>
 </ul>
-</li>
+
+To install the <strong>nightly</strong> snapshot (on a good day):
+
+<ul>
+<li>Windows or *nix users: <tt>install.packages("phylobase", repos="http://r-forge.r-project.org")</tt></li>
+<li>MacOS users: <tt>install.packages("phylobase", repos="http://r-forge.r-project.org", type = 'source')</tt></li>
+</ul>
+
+Additional information:
+<ul>
 <li>A current copy (0.3r104) of the package
-<a href="misc/phylobase.pdf">vignette</a></li>
-<li>The <strong>project summary page</strong> can found <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </li>
+<a href="misc/phylobase.pdf"><strong>vignette</strong></a></li>
+<li>The <strong>project summary page</strong> can found <a href="http://r-forge.r-project.org/projects/phylobase/"><strong>here</strong></a>. </li>
 <li>The recent TODO list can be found  <a href= "todo.html" title = "TODO"><strong>here</strong></a>. </li>
 </ul>
+
+The phylobase email lists can be accessed and subscribed to from <a href="http://r-forge.r-project.org/mail/?group_id=111">this page.</a>
 
 </body>
 </html>
