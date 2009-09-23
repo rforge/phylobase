9f1a3ffe67c5bd781e79eb9e3e196b6b4140c8f6
pdc <pdc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-23 05:07:43 +0000

    getNode should return a named vector of integers removed debugging code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylobase@661 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/edgesNAto0/R/treewalk.R
+++ branches/edgesNAto0/R/treewalk.R
@@ -9,7 +9,6 @@
 
 getNode <- function(phy, node, missing=c("warn","OK","fail")) {
     missing <- match.arg(missing)
-    browser()
     if (is.numeric(node) && all(floor(node) == node, na.rm=TRUE)) {
         node <- as.integer(node)
     }
@@ -27,16 +26,16 @@ getNode <- function(phy, node, missing=c("warn","OK","fail")) {
 
     ## node numbers
     rval <- names(labels(phy, "all"))[irval]
-    rval <- as.integer(rval)
-    browser()
-    rval[node == 0]   <- "0" # root ancestor gets special treatment
+
+    rval[node == 0]   <- 0 # root ancestor gets special treatment
     rval[is.na(node)] <- NA # return NA for any NA_character_ inputs
+    rval <- as.integer(rval)
 
     ## node labels
     nmNd <- labels(phy, "all")[irval]
 
     names(rval) <- nmNd
-    names(rval)[rval == "0"] <- "0" # root ancestor gets special treatment
+    names(rval)[rval == 0] <- "0" # root ancestor gets special treatment
 
     ## deal with nodes that don't match
     if (any(is.na(rval))) {
