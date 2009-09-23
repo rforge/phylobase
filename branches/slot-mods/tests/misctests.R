f0dacba23b384dbb000654da21a592c94f59c2f9
regetz <regetz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-23 05:47:55 +0000

    committing updated test files inadvertently left out of the last commit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylobase@664 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/slot-mods/tests/misctests.R
+++ branches/slot-mods/tests/misctests.R
@@ -38,7 +38,7 @@ p1B <- prune(p1,tip="olivacea")
 ## or ...
 p1C <- na.omit(p1)
 
-labels(p1C, "allnode") <- tolower(labels(p1C, "all"))
+labels(p1C, "all") <- tolower(labels(p1C, "all"))
 
 ## trace("prune",browser,signature="phylo4d")
 r1 <- read.tree(text="((t4:0.3210275554,(t2:0.2724586465,t3:0.2724586465):0.0485689089):0.1397952619,(t5:0.07551818331,t1:0.07551818331):0.385304634);")
